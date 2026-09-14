07ebe413cdf9081db7a72bba5d1ca7dad5ad7dc9
kashish <gkashish005@gmail.com>
2026-09-14 12:00:13 +0530

    UC3: add contacts to address book list

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using AddressBookApp.CustomException;
 using AddressBookApp.Models;
 using AddressBookApp.Validation;
+using AddressBookApp.Services;
 using System;
 class Program
 {
@@ -8,15 +9,58 @@ class Program
     {
         //Contact contact = new Contact("Kashish", "Gupta", "Bank Colony", "Bathinda", "Punjab", "151001", "89023456", "kashish@123gmail.com");
         //Console.WriteLine(contact.ToString());
-        try
+        //try
+        //{
+        //    var contact =new Contact("Kashish", "Gupta", "Bank Colony", "Bathinda", "Punjab", "151001", "8902345632", "kashish@123gmail.com");
+        //    ContactValidator.Validate(contact);
+        //    Console.WriteLine(contact.ToString());
+        //}
+        //catch(InvalidContactException ex)
+        //{
+        //    Console.WriteLine(ex.Message);
+        //}
+      AddressBook address = new AddressBook();
+        while (true)
         {
-            var contact =new Contact("Kashish", "Gupta", "Bank Colony", "Bathinda", "Punjab", "151001", "8902345632", "kashish@123gmail.com");
-            ContactValidator.Validate(contact);
-            Console.WriteLine(contact.ToString());
-        }
-        catch(InvalidContactException ex)
-        {
-            Console.WriteLine(ex.Message);
+            Console.WriteLine("---------ADDRESS BOOK MENU----------");
+            Console.WriteLine("1. Add Contact");
+            Console.WriteLine("2. Show All Contacts");
+            Console.WriteLine("0. Exit");
+            Console.WriteLine();
+            Console.WriteLine("Enter the number:");
+            int input = Convert.ToInt32(Console.ReadLine());
+            if (input == 1)
+            {
+                Console.WriteLine("Enter Your First Name: ");
+                string FirstName = Console.ReadLine()!;
+                Console.WriteLine("Enter Your Last Name: ");
+                string LastName = Console.ReadLine()!;
+                Console.WriteLine("Enter Your Address: ");
+                string Address = Console.ReadLine()!;
+                Console.WriteLine("Enter Your City: ");
+                string City = Console.ReadLine()!;
+                Console.WriteLine("Enter Your State: ");
+                string State= Console.ReadLine()!;
+                Console.WriteLine("Enter Your ZipCode: ");
+                string Zip = Console.ReadLine()!;
+                Console.WriteLine("Enter Your Phone Number: ");
+                string Phone = Console.ReadLine()!;
+                Console.WriteLine("Enter Your Email: ");
+                string Email= Console.ReadLine()!;
+
+                Contact c = new Contact(FirstName,LastName, Address, City, State, Zip, Phone, Email);
+                AddressBook.AddContact(c);
+            }
+            if(input == 2)
+            {
+                address.PrintAll();
+            }
+
+            if(input == 0)
+            {
+                return;
+            }
+
         }
 
     }
---
+++ Services/AddressBook.cs
@@ -0,0 +1,36 @@
+﻿using AddressBookApp.CustomException;
+using AddressBookApp.Models;
+using AddressBookApp.Validation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookApp.Services
+{
+    public class AddressBook
+    {
+        private static List<Contact> contacts = new();
+        public IReadOnlyList<Contact> Contacts => contacts;
+
+        public static void AddContact(Contact c)
+        {
+            try
+            {
+                ContactValidator.Validate(c);
+                contacts.Add(c);
+                Console.WriteLine("Contact Added Successfully");
+            }
+            catch(InvalidContactException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void PrintAll()
+        {
+            foreach (Contact c in contacts) { 
+                Console.WriteLine(c.ToString());
+            }
+        }
+    }
+}
